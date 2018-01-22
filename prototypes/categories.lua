33a71c45c7c3028a2458a83512fbe9156147d909
Jelmergu <jelmergu@gmail.com>
2018-01-22 19:45:53 +0100

    Changed changelog to Factorio style. This makes it possible to view the changelog on the ingame mod portal Added setting for shell icon display mode Created item group called artillery-delivery Added Bob's Ores support

--- prototypes/categories.lua
+++ prototypes/categories.lua
@@ -1,17 +1,25 @@
-data:extend(
-{
-
-  {
-    type = "ammo-category",
-    name = "artillery-delivery-shell"
-  },
-
-  {
-    type = "item-subgroup",
-    name = "artillery-delivery",
-    group = "logistics",
-    order = "f-z"
-  },
-
-}
-)
+data:extend({
+    {
+        type = "ammo-category",
+        name = "artillery-delivery-shell"
+    },
+    {
+        type = "item-group",
+        name = "artillery-delivery",
+        icon = "__ArtilleryDelivery__/graphics/icons/delivery-artillery.png",
+        icon_size = 128,
+        order = "z"
+    },
+    {
+        type = "item-subgroup",
+        name = "artillery-delivery-turrets",
+        group = "artillery-delivery",
+        order = "a"
+    },
+    {
+        type = "item-subgroup",
+        name = "artillery-delivery-shells",
+        group = "artillery-delivery",
+        order = "b"
+    }
+})
--- settings.lua
+++ settings.lua
@@ -12,3 +12,15 @@ data:extend(
 )
 
 --]]
+
+data:extend(
+    {
+        { -- added useTint as option. With a lot of ores it is harder to distinguise between different shells
+            type = "bool-setting",
+            name = "ArtilleryDelivery-useTint",
+            setting_type = "startup",
+            default_value = true,
+            order = "a"
+        }
+    }
+)
